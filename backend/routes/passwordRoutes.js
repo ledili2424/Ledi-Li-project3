7540d4e19e6dba7dd44751673d29df63830401a3
Ledi Li <ledili2424@gmail.com>
2024-04-20 20:27:13 -0700

    create PasswordShareRequest data model, and implement get all shared password as well as create new share password request

--- backend/routes/passwordRoutes.js
+++ backend/routes/passwordRoutes.js
@@ -2,24 +2,19 @@ const express = require("express");
 const verifyUser = require("./../utils/verifyUser");
 const PasswordInfo = require("../models/PasswordInfo");
 const User = require("./../models/User");
+const PasswordShareRequest = require("../models/PasswordShareRequest");
 const cookieParser = require("cookie-parser");
 
 const router = express.Router();
 router.use(cookieParser());
 
-router.get("/", verifyUser, async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.username });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    const passwordInfos = await PasswordInfo.find({ user: user._id });
-
-    return res.status(200).json(passwordInfos);
-  } catch (err) {
-    return res.status(500).json({ message: err });
+const getUser = async (username) => {
+  const user = await User.findOne({ username });
+  if (!user) {
+    throw new Error("User not found");
   }
-});
+  return user;
+};
 
 router.post("/", verifyUser, async (req, res) => {
   const { url, password } = req.body;
@@ -36,6 +31,16 @@ router.post("/", verifyUser, async (req, res) => {
     });
   } catch (err) {
     res.status(500).json({ message: "Failed to add password" });
+  }
+});
+
+router.get("/", verifyUser, async (req, res) => {
+  try {
+    const passwordInfos = await PasswordInfo.find({ user: req.id });
+
+    return res.status(200).json(passwordInfos);
+  } catch (err) {
+    return res.status(500).json({ message: err });
   }
 });
 
@@ -76,5 +81,46 @@ router.delete("/:id", verifyUser, async (req, res) => {
     });
   }
 });
+
+router.post("/share-request", verifyUser, async (req, res) => {
+  const { receiverName, url } = req.body;
+
+  try {
+    const receiver = await getUser(receiverName);
+    const passwordInfo = await PasswordInfo.findOne({ url });
+
+    const newShareRequest = await PasswordShareRequest.create({
+      sender: req.id,
+      receiver: receiver._id,
+      password: passwordInfo._id,
+      status: "not accepted",
+    });
+    res.status(200).json({
+      message: "Share request sent",
+      data: newShareRequest,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to send share request" });
+  }
+});
+
+router.get("/shared", verifyUser, async (req, res) => {
+  try {
+    const sharedRequests = await PasswordShareRequest.find({
+      receiver: req.id,
+    });
+    const sharedPasswords = await Promise.all(
+      sharedRequests.map(async (request) => {
+        const password = await PasswordInfo.findById(request.password);
+        return password;
+      })
+    );
+    return res.status(200).json(sharedPasswords.flat());
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+});
+
+
 
 module.exports = router;
