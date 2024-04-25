a67ca9a7315aaee356a2869a613d9f2918d2fd1b
Ledi Li <ledili2424@gmail.com>
2024-04-24 23:36:19 -0700

    add vite config and env variable for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: parseInt(import.meta.env.VITE_APP_PORT) || 5173,
+  },
 });
