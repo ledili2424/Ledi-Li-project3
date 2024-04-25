e38fbc10703a11387451fb1351b722c2e7584027
Ledi Li <ledili2424@gmail.com>
2024-04-25 00:47:01 -0700

    setting proxy requests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 5173
+    port: 5173,
+    minify: "terser",
+    proxy: {
+      "/api": {
+        target: "https://ledi-li-project3.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
