4e7fb5c3641a8525b2bdb3bf85a414a3555b9568
Ledi Li <ledili2424@gmail.com>
2024-04-24 23:45:42 -0700

    remove env variable

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: parseInt(import.meta.env.VITE_APP_PORT) || 5173,
+    host: "0.0.0.0"
   },
 });
