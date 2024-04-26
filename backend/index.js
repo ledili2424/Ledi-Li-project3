49b35c151152448a519fce98380bb55d6804070e
Ledi Li <ledili2424@gmail.com>
2024-04-25 21:39:48 -0700

    update

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://ledi-li-project3-fullstack.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
